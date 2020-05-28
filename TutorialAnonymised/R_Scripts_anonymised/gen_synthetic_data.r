5122bf2bd94388b0b6f575f84179d2a13c20aaa9
redraven <npr69@ncl.ac.uk>
2020-05-28 12:13:53 +0100

    tutorial anonymised

---
+++ TutorialAnonymised/R_Scripts_anonymised/gen_synthetic_data.r
@@ -0,0 +1,46 @@
+#'Author: Patricia Ryser-Welch (DataSHIELD Team)
+#'Purpose : to do ...
+#'Date of Creation : 28/5/2020
+#'Date of update:    28/5/2020
+
+
+library(readr)
+
+generate.synthetic.data <- function(dataset, name.synth.data)
+{
+    data       <- read_csv(dataset)
+    synth.GR   <- rnorm(nrow(data),mean = mean(data$GR), sd = sd(data$GR))
+    synth.GR   <- synth.GR * sign(synth.GR)
+    synth.WDS  <- as.integer(rnorm(nrow(data),mean = mean(data$WDS), sd = sd(data$WDS)))
+    synth.WDS   <- synth.WDS * sign(synth.WDS)
+    synth.YEAR <- as.integer(rnorm(nrow(data),mean = mean(data$YEAR), sd = sd(data$YEAR)))
+    synth.YEAR   <- synth.YEAR * sign(synth.YEAR)
+    synth.data <- data.frame(GR = synth.GR, WDS = synth.WDS, YEAR= synth.YEAR)
+    write.csv (synth.data, name.synth.data, row.names = FALSE)
+}
+
+verify.synthetic.data <- function(dataset, synthetic.dataset)
+{
+  original.data <- read_csv(dataset)
+  synthetic.data <- read_csv(synthetic.dataset)
+  print("Original dataset:")
+  print(summary(original.data))
+  print("Synthetic dataset:")
+  print(summary(synthetic.data))
+  
+}
+
+generate.synthetic.data("data/fully_anonymised_classic_1.csv","synth_classic_1.csv")
+generate.synthetic.data("data/fully_anonymised_classic_2.csv","synth_classic_2.csv")
+generate.synthetic.data("data/fully_anonymised_classic_3.csv","synth_classic_3.csv")
+
+verify.synthetic.data("data/fully_anonymised_classic_1.csv","synth_classic_1.csv")
+verify.synthetic.data("data/fully_anonymised_classic_2.csv","synth_classic_2.csv")
+verify.synthetic.data("data/fully_anonymised_classic_3.csv","synth_classic_3.csv")
+
+
+
+
+
+
+
--- TutorialAnonymised/R_Scripts_anonymised/main_anonymised.R
+++ TutorialAnonymised/R_Scripts_anonymised/main_anonymised.R
@@ -6,6 +6,8 @@
 
 source("R_Scripts_anonymised/anonymised_client.R")
 
+
+print("--------------- Disclosive examples ----------------")
 print("-------------- Start servers  and connect to servers -------------------")
 connections <- Connection$new()
 London <- connections$start.server()
@@ -40,5 +42,76 @@ print("----  retrieve some of the data from the servers and display them -----")
 print(connections$servers[["Newcastle"]]$datasets[["classic"]]$data[,c("Author","Title")])
 print(connections$servers[["London"]]$datasets[["classic"]]$data[,c("Author","Title","Words")])
 
+print("--------------- Less disclosive examples ----------------")
+print("-------------- Start servers  and connect to servers -------------------")
+connections <- Connection$new()
+London.less.disclosive <- connections$start.server()
+Newcastle.less.disclosive <- connections$start.server()
+Edinburgh.less.disclosive <- connections$start.server()
 
+connections$connect("London",London.less.disclosive)
+connections$connect("Newcastle",Newcastle.less.disclosive)
+connections$connect("Edinburgh",London.less.disclosive)
+
+
+
+print("-------------- upload the classic datasets -------------------")
+print("Newcastle has upload classic 1 datasets")
+path.to.data <- "data/anonymised_classic_1.csv"
+meta.data <- list("Title","GreatReadScore","Words","YearPub")
+connections$upload("Newcastle",path.to.data,meta.data,"classic")
+
+print("London has upload classic 1 datasets")
+path.to.data <- "data/anonymised_classic_2.csv"
+meta.data <- list("Title","GreatReadScore","Words","YearPub")
+connections$upload("Newcastle",path.to.data,meta.data,"classic")
+
+print("Edinbugh has upload classic 1 datasets")
+path.to.data <- "data/anonymised_classic_3.csv"
+meta.data <- list("Title","GreatReadScore","Words","YearPub")
+connections$upload("Edinburgh",path.to.data,meta.data,"classic")
+
+
+
+print("----  retrieve some of the data from the servers and display them -----")
+print(connections$servers[["Newcastle"]]$datasets[["classic"]]$data[,c("YearPub","Title")])
+print(connections$servers[["London"]]$datasets[["classic"]]$data[,c("YearPub","Title","Words")])
+print(connections$servers[["Edinburgh"]]$datasets[["classic"]]$data[,c("YearPub","Title")])
+
+
+
+print("-------------- even less disclosive examples ----------------")
+print("-------------- Start servers  and connect to servers -------------------")
+connections <- Connection$new()
+London.even.less.disclosive <- connections$start.server()
+Newcastle.even.less.disclosive <- connections$start.server()
+Edinburgh.even.less.disclosive <- connections$start.server()
+
+connections$connect("London",London.even.less.disclosive)
+connections$connect("Newcastle",Newcastle.even.less.disclosive)
+connections$connect("Edinburgh",London.even.less.disclosive)
+
+
+
+print("-------------- upload the classic datasets -------------------")
+print("Newcastle has upload classic 1 datasets")
+path.to.data <- "data/fully_anonymised_classic_1.csv"
+meta.data <- list("GreatReadScore","Words","YearPub")
+connections$upload("Newcastle",path.to.data,meta.data,"classic")
+
+print("London has upload classic 1 datasets")
+path.to.data <- "data/fully_anonymised_classic_2.csv"
+meta.data <- list("GreatReadScore","Words","YearPub")
+connections$upload("Newcastle",path.to.data,meta.data,"classic")
+
+print("Edinbugh has upload classic 1 datasets")
+path.to.data <- "data/fully_anonymised_classic_3.csv"
+meta.data <- list("GreatReadScore","Words","YearPub")
+connections$upload("Edinburgh",path.to.data,meta.data,"classic")
+
+
+print("----  retrieve some of the data from the servers and display them -----")
+print(connections$servers[["Newcastle"]]$datasets[["classic"]]$data[,c("YearPub","Words","GreatReadScore")])
+print(connections$servers[["London"]]$datasets[["classic"]]$data[,c("YearPub","Words","GreatReadScore")])
+print(connections$servers[["Edinburgh"]]$datasets[["classic"]]$data[,c("YearPub","Words","GreatReadScore")])
 

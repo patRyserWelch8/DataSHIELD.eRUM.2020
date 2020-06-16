23a3513240770cd2020cc4727e8b60632a882d53
redraven <npr69@ncl.ac.uk>
2020-06-16 13:57:16 +0100

    update

--- A. TutorialDisclosive/R_Scripts_disclosive/main_disclosive.R
+++ A. TutorialDisclosive/R_Scripts_disclosive/main_disclosive.R
@@ -14,26 +14,31 @@ Edinburgh <- connections$start.server()
 
 connections$connect("London",London)
 connections$connect("Newcastle",Newcastle)
-connections$connect("Edinburgh",London)
+connections$connect("Edinburgh",Edinburgh)
+
+
+print("-------------- upload the classic datasets -------------------")
+print("Newcastle has upload classic 1 datasets")
+path.to.data <- "data/classic_1.csv"
+meta.data <- list("Title", "Author","GreatReadScore","Words","YearPub")
+connections$upload("Newcastle",path.to.data,meta.data,"classic")
+
+print("London has upload classic w datasets")
+path.to.data <- "data/classic_2.csv"
+meta.data <- list("Title", "Author","GreatReadScore","Words","YearPub")
+connections$upload("London",path.to.data,meta.data,"classic")
+
+print("Edinburghhas upload classic 1 datasets")
+print("Edinbugh has upload classic 3 datasets")
+path.to.data <- "data/classic_3.csv"
+meta.data <- list("Title", "Author","GreatReadScore","Words","YearPub")
+connections$upload("Edinburgh",path.to.data,meta.data,"classic")
 
 
 
-print("-------------- upload all the data in the servers using the connections -------------------")
-print("The Great Fire data set is uploaded in the London Server")
-path.to.data <- "data/GreatFire.csv"
-meta.data <- list("Name","record_ID","Material", "Title","Country_pub", "Place_pub", "Publisher","Date_pub","Pages")
-connections$upload("London",path.to.data,meta.data,"GreatFire")
+print("----  retrieve some of the data from the servers and display them -----")
+print(connections$servers[["Newcastle"]]$datasets[["classic"]]$data)
+print(connections$servers[["London"]]$datasets[["classic"]]$data)
+print(connections$servers[["Edinburgh"]]$datasets[["classic"]]$data)
+#print(connections$servers[["London"]]$datasets[["classic"]]$data[,c("Author","Title","Words")])
 
-print("The Black history data set is uploaded in the Newcastle Server")
-path.to.data <- "data/BritishHistoryCleaned.csv"
-meta.data <- list("Name","Country of publication", "Place of publication","Date of publication","Pages","Languages","BMI","BMR")
-connections$upload("Newcastle",path.to.data,meta.data,"BlackHistory")
-
-
-
-print("-------------- retrieve some of the data from the servers and display data -------------------")
-names.great.fires <- connections$servers$London$datasets[["GreatFire"]]$data$Name
-print (names.great.fires)
-
-some.authors <- connections$servers$Newcastle$datasets[["BlackHistory"]]$data[, c("Name","Country of publication")]
-print(some.authors)
